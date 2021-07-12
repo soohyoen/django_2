e83744c2ab4acb3923e6b6c04de2119377d0572a
gnejf <gnejfejf789@naver.com>
2021-07-12 14:48:59 +0900

    7/12 commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,15 +7,19 @@ from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == 'POST' :
+    if request.method == 'POST':
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_output': new_hello_world})
-    else :
+                      context={'hello_world_list': hello_world_list})
+    else:
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'hello_world_list': hello_world_list})
