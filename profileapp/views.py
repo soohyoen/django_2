a35b20d46b93238a1ea8ff3cd2c946494c98b000
gnejf <gnejfejf789@naver.com>
2021-07-26 22:00:48 +0900

    7/26 commit 4

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -28,6 +30,9 @@ class ProfileUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name= 'target_profile'
